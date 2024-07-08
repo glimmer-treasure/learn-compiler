6f47ec0247c64602dd99a00c29505b709ccb5a4c
liang.cheng <glimmer.treasure@gmail.com>
2024-07-08 19:07:11 +0800

    feat: 完善scanner

--- src/scanner/index.ts
+++ src/scanner/index.ts
@@ -11,8 +11,13 @@
  * 4.字符串只支持双引号
  */
 
-import { KeyWordSet, WhiteSpaceCharSet } from "./const";
-import { isLetter, isLetterDigitOrUnderScore, isSeperator } from "./utils";
+import { KeyWordSet, WhiteSpaceCharSet } from './const'
+import {
+    isLetter,
+    isLetterDigitOrUnderScore,
+    isSeperator,
+    isDigit,
+} from './utils'
 
 // Token类型
 export enum TokenKind {
@@ -30,8 +35,8 @@ export enum TokenKind {
 
 // 代表一个Token的数据结构
 export interface Token {
-    kind: TokenKind;
-    text: string;
+    kind: TokenKind
+    text: string
 }
 
 /**
@@ -41,33 +46,37 @@ export interface Token {
  * eof():判断是否已经到了结尾。
  */
 export class CharStream {
-    data: String = "";
-    pos: number = 0;
-    line: number = 1;
-    col: number = 0;
+    data: String = ''
+    pos: number = 0
+    line: number = 1
+    col: number = 0
 
     constructor(data: string) {
-        this.data = data;
+        this.data = data
     }
 
     peek(): string {
-        return this.data.charAt(this.pos);
+        return this.data.charAt(this.pos)
     }
 
     next(): string {
-        let ch = this.data.charAt(this.pos);
-        this.pos += 1;
-        if (ch == "\n") {
-            this.line += 1;
-            this.col = 0;
+        let ch = this.data.charAt(this.pos)
+        this.pos += 1
+        if (ch == '\n') {
+            this.line += 1
+            this.col = 0
         } else {
-            this.col += 1;
+            this.col += 1
         }
-        return ch;
+        return ch
     }
 
     isEof(): boolean {
-        return this.peek() == "";
+        return this.peek() === ''
+    }
+
+    isNotEof(): boolean {
+        return !this.isEof()
     }
 }
 
@@ -80,12 +89,11 @@ export class CharStream {
  * peek2(): 预读第二个Token。
  */
 export class Scanner {
-    tokens: Array<Token> = new Array<Token>(); //采用一个array，能预存多个Token.
-    stream: CharStream;
-    private static KeyWords: Set<string> = new Set([]);
+    tokens: Array<Token> = new Array<Token>() //采用一个array，能预存多个Token.
+    stream: CharStream
 
     constructor(stream: CharStream) {
-        this.stream = stream;
+        this.stream = stream
     }
 
     /**
@@ -93,92 +101,380 @@ export class Scanner {
      */
     private skipWhiteSpaces() {
         while (WhiteSpaceCharSet.has(this.stream.peek())) {
-            this.stream.next();
+            this.stream.next()
         }
     }
+
+    /**
+     * 跳过多行注释
+     */
+    private skipMultipleLineComments() {
+        //跳过*，/之前已经跳过去了。
+        let ch1 = this.stream.next()
+        while (!this.stream.isEof()) {
+            let ch2 = this.stream.next()
+            if (ch1 === '*' && ch2 === '/') {
+                return
+            }
+            ch1 = ch2
+        }
+        //如果没有匹配上，报错。
+        console.error(
+            "Failed to find matching */ for multiple line comments at ': " +
+                this.stream.line +
+                ' col: ' +
+                this.stream.col,
+        )
+    }
+
+    /**
+     * 跳过多行注释
+     */
+    private skipSingleLineComment() {
+        //跳过第二个/，第一个之前已经跳过去了。
+        this.stream.next()
+        while (this.stream.isNotEof() && this.stream.peek() !== '\n') {
+            this.stream.next()
+        }
+    }
+
     /**
      * 解析标识符，标识符和关键字统一用此方法解析
      */
     private parseIdentifer() {
-        let token: Token = { kind: TokenKind.Identifier, text: "" };
+        let token: Token = { kind: TokenKind.Identifier, text: '' }
         //第一个字符不用判断，因为在调用者那里已经判断过了
-        token.text += this.stream.next();
+        token.text += this.stream.next()
         //读入后序字符
         while (
             !this.stream.isEof() &&
             isLetterDigitOrUnderScore(this.stream.peek())
         ) {
-            token.text += this.stream.next();
+            token.text += this.stream.next()
         }
 
-        if (token.text === "null") {
-            token.kind = TokenKind.NullLiteral;
-        } else if (["true", "false"].includes(token.text)) {
-            token.kind = TokenKind.BooleanLiteral;
+        if (token.text === 'null') {
+            token.kind = TokenKind.NullLiteral
+        } else if (['true', 'false'].includes(token.text)) {
+            token.kind = TokenKind.BooleanLiteral
         } else if (KeyWordSet.has(token.text)) {
             //识别出关键字（从字典里查，速度会比较快）
-            token.kind = TokenKind.KeyWord;
+            token.kind = TokenKind.KeyWord
         }
-        return token;
+        return token
     }
 
     private parseStringLiteral() {
-        let token: Token = { kind: TokenKind.StringLiteral, text: "" };
+        let token: Token = { kind: TokenKind.StringLiteral, text: '' }
         //第一个字符不用判断，因为在调用者那里已经判断过了
-        this.stream.next();
-        while(!this.stream.isEof() && this.stream.peek() !== '"') {
+        this.stream.next()
+        while (!this.stream.isEof() && this.stream.peek() !== '"') {
             token.text += this.stream.next()
         }
         if (this.stream.peek() === '"') {
-            this.stream.next();
+            this.stream.next()
         } else {
-            console.error("Expecting an \" at line: " + this.stream.line + " col: " + this.stream.col);
+            console.error(
+                'Expecting an " at line: ' +
+                    this.stream.line +
+                    ' col: ' +
+                    this.stream.col,
+            )
         }
-        return token;
+        return token
     }
 
     //从字符串流中获取一个新Token。
     private getAToken(): Token {
-        this.skipWhiteSpaces();
+        this.skipWhiteSpaces()
         if (this.stream.isEof()) {
-            return { kind: TokenKind.EOF, text: "" };
+            return { kind: TokenKind.EOF, text: '' }
         } else {
-            let ch = this.stream.peek();
-            if (isLetter(ch) || ch === "_") {
-                return this.parseIdentifer();
+            let ch = this.stream.peek()
+            if (isLetter(ch) || ch === '_') {
+                return this.parseIdentifer()
             } else if (ch === '"') {
-                return this.parseStringLiteral();
+                return this.parseStringLiteral()
             } else if (isSeperator(ch)) {
                 this.stream.next()
-                return {kind:TokenKind.Seperator,text:ch}
+                return { kind: TokenKind.Seperator, text: ch }
+            } else if (isDigit(ch)) {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                let literal: string = ''
+                if (ch === '0') {
+                    if (!(ch1 >= '1' && ch1 <= '9')) {
+                        literal = '0'
+                    } else {
+                        console.log(
+                            '0 cannot be followed by other digit now, at line: ' +
+                                this.stream.line +
+                                ' col: ' +
+                                this.stream.col,
+                        )
+                    }
+                } else if (ch >= '1' && ch <= '9') {
+                    literal += ch
+                    while (isDigit(ch1)) {
+                        ch = this.stream.next()
+                        literal += ch
+                        ch1 = this.stream.peek()
+                    }
+                }
+                if (ch1 === '.') {
+                    literal += '.'
+                    this.stream.next()
+                    ch1 = this.stream.peek()
+                    while (isDigit(ch1)) {
+                        ch = this.stream.next()
+                        literal += ch
+                        ch1 = this.stream.peek()
+                    }
+                    return { kind: TokenKind.DecimalLiteral, text: literal }
+                } else {
+                    //返回一个整型直面量
+                    return { kind: TokenKind.IntegerLiteral, text: literal }
+                }
+            } else if (ch === '.') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (isDigit(ch1)) {
+                    let literal = '.'
+                    while (isDigit(ch1)) {
+                        ch = this.stream.next()
+                        literal += ch
+                        ch1 = this.stream.peek()
+                    }
+                    return { kind: TokenKind.DecimalLiteral, text: literal }
+                } else if (ch1 === '.') {
+                    // 解析省略号 ‘...’
+                    this.stream.next()
+                    ch1 = this.stream.peek()
+                    if (ch1 == '.') {
+                        return { kind: TokenKind.Seperator, text: '...' }
+                    } else {
+                        console.error('Unrecognized pattern : .., missed a . ?')
+                        return this.getAToken()
+                    }
+                } else {
+                    //.号分隔符
+                    return { kind: TokenKind.Seperator, text: '.' }
+                }
+            } else if (ch === '/') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '*') {
+                    this.skipMultipleLineComments()
+                    return this.getAToken()
+                } else if (ch1 === '/') {
+                    this.skipSingleLineComment()
+                    return this.getAToken()
+                } else if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '/=' }
+                } else {
+                    return { kind: TokenKind.Operator, text: '/' }
+                }
+            } else if (ch === '+') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '+') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '++' }
+                } else if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '+=' }
+                } else {
+                    return { kind: TokenKind.Operator, text: '+' }
+                }
+            } else if (ch === '-') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                switch (ch1) {
+                    case '-':
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '--' }
+                    case '=':
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '-=' }
+                    default:
+                        return { kind: TokenKind.Operator, text: '-' }
+                }
+            } else if (ch === '*') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                switch (ch1) {
+                    case '=':
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '*=' }
+                    default:
+                        return { kind: TokenKind.Operator, text: '*' }
+                }
+            } else if (ch === '%') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                switch (ch1) {
+                    case '=':
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '%=' }
+                    default:
+                        return { kind: TokenKind.Operator, text: '%' }
+                }
+            } else if (ch === '>') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '>=' }
+                } else if (ch1 === '>') {
+                    this.stream.next()
+                    let ch2 = this.stream.peek()
+                    if (ch2 === '>') {
+                        this.stream.next()
+                        let ch3 = this.stream.peek()
+                        if (ch3 === '=') {
+                            this.stream.next()
+                            return { kind: TokenKind.Operator, text: '>>>=' }
+                        } else {
+                            return { kind: TokenKind.Operator, text: '>>>' }
+                        }
+                    } else if (ch2 === '=') {
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '>>=' }
+                    } else {
+                        return { kind: TokenKind.Operator, text: '>>' }
+                    }
+                } else {
+                    return { kind: TokenKind.Operator, text: '>' }
+                }
+            } else if (ch === '<') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '<=' }
+                } else if (ch1 === '<') {
+                    this.stream.next()
+                    let ch2 = this.stream.peek()
+                    if (ch2 === '=') {
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '<<=' }
+                    } else {
+                        return { kind: TokenKind.Operator, text: '<<' }
+                    }
+                } else {
+                    return { kind: TokenKind.Operator, text: '<' }
+                }
+            } else if (ch === '=') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '=') {
+                    this.stream.next()
+                    let ch2 = this.stream.peek()
+                    if (ch2 === '=') {
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '===' }
+                    } else {
+                        return { kind: TokenKind.Operator, text: '==' }
+                    }
+                } else if (ch1 === '>') {
+                    // 箭头=>
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '=>' }
+                } else {
+                    return { kind: TokenKind.Operator, text: '=' }
+                }
+            } else if (ch === '!') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '=') {
+                    this.stream.next()
+                    let ch2 = this.stream.peek()
+                    if (ch2 === '=') {
+                        this.stream.next()
+                        return { kind: TokenKind.Operator, text: '!==' }
+                    } else {
+                        return { kind: TokenKind.Operator, text: '!=' }
+                    }
+                } else {
+                    return { kind: TokenKind.Operator, text: '!' }
+                }
+            } else if (ch === '|') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '|') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '||' }
+                } else if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '|=' }
+                } else {
+                    return { kind: TokenKind.Operator, text: '|' }
+                }
+            } else if (ch === '&') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '&') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '&&' }
+                } else if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '&=' }
+                } else {
+                    return { kind: TokenKind.Operator, text: '&' }
+                }
+            } else if (ch === '^') {
+                this.stream.next()
+                let ch1 = this.stream.peek()
+                if (ch1 === '=') {
+                    this.stream.next()
+                    return { kind: TokenKind.Operator, text: '^=' }
+                } else {
+                    return { kind: TokenKind.Operator, text: '^' }
+                }
+            } else if (ch === '~') {
+                this.stream.next()
+                return { kind: TokenKind.Operator, text: '~' }
+            } else {
+                //暂时去掉不能识别的字符
+                console.error(
+                    "Unrecognized pattern meeting ': " +
+                        ch +
+                        "', at" +
+                        this.stream.line +
+                        ' col: ' +
+                        this.stream.col,
+                )
+                this.stream.next()
+                return this.getAToken()
             }
         }
     }
 
     next(): Token {
-        let t: Token | undefined = this.tokens.shift();
-        if (typeof t == "undefined") {
-            return this.getAToken();
+        let t: Token | undefined = this.tokens.shift()
+        if (typeof t == 'undefined') {
+            return this.getAToken()
         } else {
-            return t;
+            return t
         }
     }
 
     peek(): Token {
-        let t: Token | undefined = this.tokens[0];
-        if (typeof t == "undefined") {
-            t = this.getAToken();
-            this.tokens.push(t);
+        let t: Token | undefined = this.tokens[0]
+        if (typeof t == 'undefined') {
+            t = this.getAToken()
+            this.tokens.push(t)
         }
-        return t;
+        return t
     }
 
     peek2(): Token {
-        let t: Token | undefined = this.tokens[1];
-        while (typeof t == "undefined") {
-            this.tokens.push(this.getAToken());
-            t = this.tokens[1];
+        let t: Token | undefined = this.tokens[1]
+        while (typeof t == 'undefined') {
+            this.tokens.push(this.getAToken())
+            t = this.tokens[1]
         }
-        return t;
+        return t
     }
 }
--- src/scanner/utils.ts
+++ src/scanner/utils.ts
@@ -1,21 +1,21 @@
-import {SeperatorSet} from './const'
+import { SeperatorSet } from './const'
 
 /**
  * 是否是字母
  */
 export const isLetter = (ch: string) =>
-    (ch >= "A" && ch <= "Z") || (ch >= "a" && ch <= "z");
+    (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')
 
 /**
  * 是否是数字
  */
-export const isDigit = (ch: string) => ch >= "0" && ch <= "9";
+export const isDigit = (ch: string) => ch >= '0' && ch <= '9'
 
 /**
  * 是否是字母、数字或者下划线
  */
 export const isLetterDigitOrUnderScore = (ch: string) =>
-    isLetter(ch) || isDigit(ch) || ch === "_";
+    isLetter(ch) || isDigit(ch) || ch === '_'
 
 /**
  * 是否是分隔符
